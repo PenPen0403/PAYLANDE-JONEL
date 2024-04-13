a45093011f31be373ed5a4a9cd6d484359f53c44
Jonel Paylande <jonelpaylande0403@gmail.com>
2024-04-13 21:26:06 +0800

    COMMIH

--- src/app/calculator/calculator.page.ts
+++ src/app/calculator/calculator.page.ts
@@ -7,20 +7,50 @@ import { AuthenticationService } from '../authentication.service';
   styleUrls: ['./calculator.page.scss'],
 })
 export class CalculatorPage implements OnInit {
- input: string= '';
- history: string = '';
- fNumber: number = 0;
- sNumber: number= 0;
- equalsTo: boolean = false;
- strr: string = '';
- click: boolean = true;
- unclick: boolean=false;
+input: string = '';
+output: string= '';
+showHistory: boolean = false;
+history : {input: string , calculated: string} | null = null;
+username: string | null= '';
 
-  constructor() { }
+  constructor(private auth: AuthenticationService) { }
 
   ngOnInit() {
+    this.auth.authenticated = false;
+    this.username= localStorage.getItem('name');
   }
 
-  
+  addNum(num: string){
+    if(num=== '+'|| num === '-'|| num==='/'|| num === '*'){
+      if(this.input.length === 0 || this.isOperator(this.input[this.input.length - 1])){
+        return;
+      }
+    }
+    this.input += num;
+  }
+
+  isOperator(opr: string){
+    return opr === '+' || opr === '-' || opr === '/' || opr === '*';
+  }
+
+  clearInput(){
+    this.input='';
+  }
+
+  calculate(){
+    try{
+      const calculated = eval(this.input);
+      this.history={input: this.input, calculated: calculated.toString()};
+      this.input = calculated.toString();
+      this.showHistory=true;
+      this.clearInput();
+    } catch (error){
+      this.input = 'Error, Click C to reset';
+    }
+  }
+
+
+
+
 
 }
