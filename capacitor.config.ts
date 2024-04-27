c9ca3f3d34e511581230654e4e5c1973e0e36802
Jonel Paylande <jonelpaylande0403@gmail.com>
2024-04-27 08:17:03 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ACTIVITY_9_P',
+  webDir: 'www'
+};
+
+export default config;
